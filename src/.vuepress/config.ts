42f88b35b008786281078c3c24f3e89fd0f5f728
WuWenlong <7733141621@qq.com>
2024-10-17 17:34:47 +0800

    test

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/ron-blog/",
 
   lang: "zh-CN",
   title: "Ron 个人博客",
